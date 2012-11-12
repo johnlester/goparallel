0048c531d8d7fd8b08b372b96372bff0b5141e40
John Lester <johnlester@gmail.com>
2012-11-11 23:25:05 -0500

    It works!

--- goparallel.go
+++ goparallel.go
@@ -1,23 +1,154 @@
 package goparallel
 
 import (
-//	"math"
-//	"fmt"
-//	"math/rand"
-//	"log"
-//	"runtime"
+	"fmt"
+	"runtime"
+	"math/big"
+	// "time"
+	//	"math"
+	//	"math/rand"
+	//	"log"
 )
 
 var (
+	// numCores = runtime.NumCPU()
 )
 
 const (
 )
 
-type InputItem interface{}
-type OutputItem interface{}
-type Task func(input InputItem) OutputItem
+// type InputItem interface{}
+// type OutputItem interface{}
 
-func DoParallel(task Task, inputs []InputItem, maxCores int) []OutputItem {
-	
+type InputItem struct {
+	n *big.Int
 }
+
+type Job struct {
+    jobID int
+	input InputItem
+	resultsChan  chan<- Result
+}	
+
+type OutputItem struct {
+	n *big.Int
+	factors []*big.Int
+}
+
+type Result struct {
+	jobID int
+	output OutputItem
+}
+
+
+
+//Main function to do parallel computation
+func DoParallel(inputs []InputItem, maxWorkers int) []OutputItem {
+	
+	//set num of workers
+	numWorkers := maxWorkers
+	
+	fmt.Printf("Using %v of %v cores, with %v workers\n", runtime.GOMAXPROCS(0), runtime.NumCPU(), numWorkers)
+
+	numOutputs := len(inputs)
+	
+	//Create channels
+    jobsChan := make(chan Job, numWorkers)
+    resultsChan := make(chan Result, len(inputs))
+    doneChan := make(chan struct{}, numWorkers)
+
+    go addJobs(jobsChan, inputs, resultsChan) // Executes in its own goroutine
+    for i := 0; i < numWorkers; i++ {
+        go doJobs(doneChan, jobsChan) // Each executes in its own goroutine
+    }
+    go awaitCompletion(doneChan, resultsChan, numWorkers) // Executes in its own goroutine
+    finalOutputs := processResults(resultsChan, numOutputs)           // Blocks until the work is done
+	return finalOutputs
+}
+
+func addJobs(jobsChan chan<- Job, inputs []InputItem, resultsChan chan<- Result) {
+    for i, input := range inputs {
+        jobsChan <- Job{i, input, resultsChan}
+    }
+    close(jobsChan)
+}
+
+func doJobs(doneChan chan<- struct{}, jobsChan <-chan Job) {
+    for job := range jobsChan {
+        job.Do()	//job.Do() doesn't need any parameters in this case
+    }
+    doneChan <- struct{}{}
+}
+
+func (job Job) Do() {
+	output := OutputItem{job.input.n, factorsBig(job.input.n)}
+	job.resultsChan <- Result{job.jobID, output}
+}
+
+func awaitCompletion(doneChan <-chan struct{}, resultsChan chan Result, numWorkers int) {
+	for i := 0; i < numWorkers; i++ {
+		<-doneChan
+	}
+	close(resultsChan)
+}
+
+func processResults(resultsChan <-chan Result, numOutputs int) []OutputItem {
+	finalOutputs := make([]OutputItem, numOutputs)
+	for result := range resultsChan {
+		fmt.Printf("%v) %v: %v\n", result.jobID, result.output.n, result.output.factors)
+		finalOutputs[result.jobID] = result.output
+	}
+	return finalOutputs
+}
+
+
+//////////////////////////////////////////////////
+// Factoring functions
+//////////////////////////////////////////////////
+
+// Sqrt for Big numbers
+func sqrtBig(n *big.Int) (x *big.Int) {
+	switch n.Sign() {
+	case -1:
+		panic(-1)
+	case 0:
+		return big.NewInt(0)
+	}
+
+	var px, nx big.Int
+	x = big.NewInt(0)
+	x.SetBit(x, n.BitLen()/2+1, 1)
+	for {
+		nx.Rsh(nx.Add(x, nx.Div(n, x)), 1)
+		if nx.Cmp(x) == 0 || nx.Cmp(&px) == 0 {
+			break
+		}
+		px.Set(x)
+		x.Set(&nx)
+	}
+	return
+}
+
+func factorsBig(n *big.Int) []*big.Int {
+	factors := make([]*big.Int, 2, 10)
+	factors[0] = big.NewInt(1)
+	factors[1] = n
+	zero := big.NewInt(0)
+	one := big.NewInt(1)
+	i := big.NewInt(2)
+	sqrtN := sqrtBig(n)
+	for i.Cmp(sqrtN) < 1 {
+		z := big.NewInt(0)
+		m := big.NewInt(0)
+		div := big.NewInt(0)
+		div = div.Add(div, i)
+		z, m = z.DivMod(n, div, m)
+		if m.Cmp(zero) == 0	{   //modulus equals zero
+			factors = append(factors, div)
+			factors = append(factors, z)
+		}
+		i = i.Add(i, one)
+	}
+	return factors
+}
+
---
+++ goparallel_test.go
@@ -0,0 +1,89 @@
+package goparallel
+
+import (
+	"testing"
+	"math/big"
+	"fmt"
+	"runtime"
+)
+
+const (
+	bigsToFactor = 100
+	// startingBigString = "1234567891011121314151617181920"
+	startingBigString = "248163264128"
+)
+
+var (
+	bigOne = big.NewInt(1)
+	bigTwo = big.NewInt(2)
+)
+
+
+/////////////////////////////////////////////////////////////
+// goParallel tests
+/////////////////////////////////////////////////////////////
+
+func TestGoParallel(t *testing.T) {
+
+	//Create big to start
+	start := new(big.Int)
+	_, err := fmt.Sscan(startingBigString, start)	
+	if err != nil {
+		t.Errorf("error scanning big string:", err)
+	}
+
+	//Create inputs
+	inputs := make([]InputItem, bigsToFactor)
+	for i := 0; i < bigsToFactor; i++ {
+		nextBig := big.NewInt(int64(i))
+		nextBig = nextBig.Add(nextBig, start)
+		inputs[i] = InputItem{nextBig}
+	}
+
+	//Do computation in parallel
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	var outputs []OutputItem
+	outputs = DoParallel(inputs, 1)
+	
+	//Tests
+	if len(inputs) != len(outputs) {
+		t.Errorf("inputs (%v) should have same len as outputs (%v)", len(inputs), len(outputs))
+	}
+
+}
+
+
+
+
+/////////////////////////////////////////////////////////////
+// bif factor tests
+/////////////////////////////////////////////////////////////
+
+func TestGoParallel_Sqrt(t *testing.T) {
+	four := big.NewInt(4)
+	two := big.NewInt(2)
+	if sqrtBig(four).Cmp(two) != 0 {
+		t.Errorf("mo money, mo problems")
+	}
+	
+	twentyFive := big.NewInt(25)
+	five := big.NewInt(5)
+	if sqrtBig(twentyFive).Cmp(five) != 0 {
+		t.Errorf("mo money, mo problems")
+	}
+	
+	oneOhOne := big.NewInt(101)
+	ten := big.NewInt(10)
+	if sqrtBig(oneOhOne).Cmp(ten) != 0 {
+		t.Errorf("mo money, mo problems")
+	}
+
+	z := big.NewInt(0)
+	m := big.NewInt(0)
+	z, m = z.DivMod(oneOhOne, ten, m)
+	if m.Cmp(bigOne) != 0 {
+		t.Errorf("mo money, mo problems")
+	}
+
+}
+
